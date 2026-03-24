27eb609371f117a88e4be7fb9b49467c74cb47fa
Samin Rahman <55238473+hornet67@users.noreply.github.com>
2026-03-24 11:27:56 +0600

    postman

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from . import views
+from .views import *
 
 
 urlpatterns = [
-    path('', views.home, name='home'), 
-]
+        path('', home, name='home'), 
+        path('data/', data1, name='data'), 
+    ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse({
+    return JsonResponse({
         'message': 'Hello, World!',
-        'name': 'John Doe'
+        'name': 'John Doe',
+        'learning': True
+        
     })
 
+
+
+def data1(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        name = data.get('name')
+        age = data.get('age')
+        return JsonResponse({
+            'message': 'Hello, World!',
+            'name': 'John Doe',
+            'learning': True,
+            'name': name,
+            'age': age
+            
+        })
+    return JsonResponse({'error': 'Only POST allowed'}, status=400)
+        
 
 
