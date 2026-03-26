06ddbae7d5beb79837d0c9b9e82053a51d6e668a
Samin Rahman <55238473+hornet67@users.noreply.github.com>
2026-03-26 16:04:21 +0600

    working on  new channel

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,5 @@ from .views import *
 
 
 urlpatterns = [
-        path('', home, name='home'), 
-        path('data/', data1, name='data'), 
+        path('', chat_view), 
     ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,30 +4,9 @@ import json
 # Create your views here.
 
 
-def home(request):
-    return JsonResponse({
-        'message': 'Hello, World!',
-        'name': 'John Doe',
-        'learning': True
-        
-    })
 
-
-
-def data1(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        name = data.get('name')
-        age = data.get('age')
-        return JsonResponse({
-            'message': 'Hello, World!',
-            'name': 'John Doe',
-            'learning': True,
-            'name': name,
-            'age': age
-            
-        })
-    return JsonResponse({'error': 'Only POST allowed'}, status=400)
+def chat_view(request):
+    return render(request, 'index.html')
         
 
 
